12ec66bfc28d59fc26efce20161f724af96651bd
rafhabs <rafhabs@gmail.com>
2018-11-08 01:38:39 +0000

    Setting database password in the database.yml

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
-
+    namespace :v1 do
+        
+    end
   end
 end

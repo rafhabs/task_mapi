fa7d5f7104c91ed7903e255a0715adef69f631b3
rafhabs <rafhabs@gmail.com>
2018-11-11 22:43:35 +0000

    Additing should a matcher for user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
-  before { @user = FactoryGirl.build(:user) }
 
-  it { expect(@user).to respond_to(:email) }
+  let(:user) { build(:user) }
 
-  it { expect(@user).to respond_to(:name) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_confirmation_of(:password) }
+  it { is_expected.to allow_value("rafhabs@gmail.com").for(:email) }
 
-  it { expect(@user).to respond_to(:password) }
 
-  it { expect(@user).to respond_to(:password_confirmation) }
-
-  it { expect(@user).to be_valid }
 
 
 end
